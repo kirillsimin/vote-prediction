26bf6f3db2753379b9559d63dab17059eac2cc5e
Kirill Simin <kirillsimin@pm.me>
2023-04-01 13:18:15 -0400

    turns data gathering into class, adds backoff for fetching full text

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ import numpy as np
 from datasets import Dataset, DatasetDict
 import requests
 from bs4 import BeautifulSoup
+import os
 import time
 
 class Data:
@@ -14,6 +15,7 @@ class Data:
                                 password="legiscan",
                                 port="5432")
         self.cursor = self.conn.cursor()
+        self.wait = 0
 
     def get_people(self):
         self.cursor.execute("SELECT people_id, name from ls_people order by people_id;")
@@ -23,24 +25,36 @@ class Data:
 
     def get_votes(self):
         """ get votes and description """
-        self.cursor.execute("SELECT lb.description as text, string_agg(lp.people_id::text, ',') as label FROM ls_bill lb join ls_bill_vote lbv on lbv.bill_id=lb.bill_id join ls_bill_vote_detail lbvd on lbv.roll_call_id=lbvd.roll_call_id join ls_people lp on lbvd.people_id=lp.people_id where lbvd.vote_id = 1 group by lb.bill_id;")
+        self.cursor.execute("SELECT lb.bill_id as bill_id, string_agg(lp.people_id::text, ',') as label FROM ls_bill lb join ls_bill_vote lbv on lbv.bill_id=lb.bill_id join ls_bill_vote_detail lbvd on lbv.roll_call_id=lbvd.roll_call_id join ls_people lp on lbvd.people_id=lp.people_id where lbvd.vote_id = 1 group by lb.bill_id;")
         votes = pd.DataFrame(self.cursor.fetchall())
-        votes = votes.rename(columns={0: "text", 1: "people"})
+        votes = votes.rename(columns={0: "bill_id", 1: "people"})
         votes["labels"] = None
         return votes
 
 
     def transpose_votes_to_labels(self, votes, people):
+        print(votes)
         for index, row in votes.iterrows():
+            full_text = ""
+            try:
+                file_name = './full_text/'+str(row[0])+'.txt'
+                with open(file_name, 'r') as file:
+                    full_text = file.read()
+            except FileNotFoundError:
+                print(str(row[0]) + " not found.")
+
             labels = []
             ids_voted = row["people"].split(',')
             for people_id in people[0]:
                 labels.append(float(1) if str(people_id) in ids_voted else float(0))
             
             votes.at[index, "labels"] = labels
+            votes.at[index, "text"] = full_text
 
         votes = votes.fillna(0)
         votes = votes.drop("people", axis=1)
+        votes = votes.drop("bill_id", axis=1)
+        votes = votes[["text", "labels"]]
         
         print(votes)
         return votes
@@ -69,16 +83,39 @@ class Data:
 
     def fetch_full_text(self, links):
         for index, row in links.iterrows():
+            self.fetch_one_bill_text(row)
+
+    def fetch_one_bill_text(self, row):
             bill_id = row[0]
             text_link = row[1]
+            file_name = './full_text/'+str(bill_id)+'.txt'
             print(text_link)
-            time.sleep(1.5)
-            response = requests.get(text_link)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            text = soup.find("div", class_="document").get_text().strip()
+            if os.path.isfile(file_name):
+                return
 
-            with open('./full_text/'+str(bill_id)+'.txt', 'w') as output_file:
-                output_file.write(text)
+            try:
+                time.sleep(self.wait)
+                response = requests.get(text_link)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                text = soup.find("body").get_text().strip()
+
+                with open(file_name, 'w') as output_file:
+                    output_file.write(text)
+                
+                self.wait = 0
+            except requests.exceptions.ConnectionError:
+                if self.wait > 63:
+                    print("Request ERROR, wait is more than 63")
+                    raise Exception
+
+                if self.wait == 0:
+                    self.wait = 1
+                else:
+                    self.wait = self.wait * 2
+                print("Request ERROR; sleeping for " + str(self.wait))
+                self.fetch_one_bill_text(row)
+
+
 
 if __name__ == "__main__":
     data = Data()
@@ -86,8 +123,8 @@ if __name__ == "__main__":
     links = data.get_full_text_links()
     data.fetch_full_text(links)
 
-    # people = data.get_people()
-    # votes = data.get_votes()
-    # votes_with_labels = data.transpose_votes_to_labels(votes, people)
-    # data.create_data_set(votes_with_labels)
+    people = data.get_people()
+    votes = data.get_votes()
+    votes_with_labels = data.transpose_votes_to_labels(votes, people)
+    data.create_data_set(votes_with_labels)
 
